006fd89121571fea35055d6700622d045193c369
Krystian Bieniek <krystekk2013@gmail.com>
2019-02-08 17:28:09 +0100

    Add more info in html css js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
+var capitalList = $('#capitals');
+var areaList = $('#areas');
+var populationList = $('#populations');
+var languageList = $('#languages');
+var currencyList = $('#currencies');
 
 $('#search').click(searchCountries);
 
@@ -9,13 +14,21 @@ function searchCountries() {
   $.ajax({
     url: url + countryName,
     method: 'GET',
-    success: showCountriesList
+    success: function showCountriesTable (resp) {
+      countriesList.empty();
+      capitalList.empty();
+      areaList.empty();
+      populationList.empty();
+      languageList.empty();
+      currencyList.empty();
+      resp.forEach(function(item) {
+        $('<tr>').text(item.name).appendTo(countriesList);
+        $('<tr>').text(item.capital).appendTo(capitalList);
+        $('<tr>').text(item.area).appendTo(areaList);
+        $('<tr>').text(item.population).appendTo(populationList);
+        $('<tr>').text(item.languages).appendTo(languageList);
+        $('<tr>').text(item.currencies).appendTo(currencyList);
+      });
+    }
   });
-
-  function showCountriesList(resp) {
-    countriesList.empty();
-    resp.forEach(function(item) {
-      $('<li>').text(item.name).appendTo(countriesList);
-    });
-  };
 }
